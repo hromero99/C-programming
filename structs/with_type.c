8e47b849765df59cfe145f61924acb96c17259f3
Héctor Romero López <cyberh99@gmail.com>
2018-03-14 22:52:52 +0100

    Struct examples with typedef done

---
+++ structs/with_type.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "with_type_header.h"
+
+
+int main(int argc, char const *argv[]) {
+  monomio p;
+  p.grado = 2;
+  printf("%d\n",p.grado );
+  return 0;
+}
