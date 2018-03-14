0be8f08eedd9340b4a406df58454b52dc84f7b38
Héctor Romero López <cyberh99@gmail.com>
2018-03-14 18:52:05 +0100

    Header file modify

--- p1/monomio/functions.h
+++ p1/monomio/functions.h
@@ -1,4 +1,4 @@
-
+#ifndef FUNCTIONS_H
 #define SIZE 2
 
 typedef struct s_monomio{
@@ -18,3 +18,5 @@ monomio* mayor_menor_grado(monomio* pol);
 void reading_vector(monomio* pol);
 
 void calcular_valor(monomio* pol);
+
+#endif
