01b3a54703bb2f1764ef946dbf039d1d2965e31d
Héctor Romero López <cyberh99@gmail.com>
2018-03-15 07:55:00 +0100

    Ejemplo del operador flecha agregado

--- structs/with_type.c
+++ structs/with_type.c
@@ -3,9 +3,17 @@
 #include "with_type_header.h"
 
 
+void pol_f(monomio* pol){
+//No sirve el operador . tiene que hacerse uso del operador ->
+  pol->grado = 2;
+  printf("%d\n",pol->grado );
+}
+
 int main(int argc, char const *argv[]) {
   monomio p;
   p.grado = 2;
   printf("%d\n",p.grado );
+  monomio pol[2];
+  pol_f(pol);
   return 0;
 }
