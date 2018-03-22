be7da9c677efa5d8ee64c1f7b20706aa0fda1007
Héctor Romero López <cyberh99@gmail.com>
2018-03-22 09:36:40 +0100

    Exercise 2 done

---
+++ p2/ej2/funciones.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int * buildArray(int size){
+  int* pArray;
+  pArray = calloc(size, sizeof(int ));
+  return pArray;
+}
+
+void fillArray(int array[], int size){
+  srand(time(NULL));
+  for ( int i = 0;i<size;i++){
+    *(array+i) = rand()%20;
+
+
+  }
+}
+
+void writeArray(int array[], int index,int valor){
+    *(array+index) =valor;
+}
+
+int ArrayValues(int *vector,int size_vector, int num, int *mayores, int *menores,int* minAux,int* maxAux){
+
+  for (int i = 0; i< size_vector;i++){
+    if (*(vector + i) > num){
+      writeArray(mayores,*maxAux,(*(vector+i)));
+      *maxAux = *maxAux +1 ;
+    }
+    else{
+      writeArray(menores,*minAux,*(vector+i));
+      *minAux = *minAux +1 ;
+
+    }
+  }
+  mayores = realloc(mayores, *maxAux * sizeof(int));
+  menores = realloc(menores, *minAux * sizeof(int));
+
+}
+
+void printValues(int array[], int values){
+  for (int i = 0; i<values;i++){
+    printf("%d\t", *(array+i));
+  }
+}
